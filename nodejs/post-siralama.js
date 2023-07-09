1ed0342274d492ca4639639ee65dd6f646bc491b
Furkan Özay <53002170+FurkanOzay@users.noreply.github.com>
2023-07-09 03:10:34 +0300

    Update post-siralama.js

--- nodejs/post-siralama.js
+++ nodejs/post-siralama.js
@@ -1,3 +1,4 @@
+//1
 const posts = [
     {title : "Post 1"},
     {title: "Post 2"},
@@ -16,3 +17,37 @@ const addPost = (post, callback) => {
 }
 
 addPost({title : "Post 4"}, listPost)
+
+//2
+const posts = [
+    { title: "Post 1" },
+    { title: "Post 2" },
+    { title: "Post 3" }
+]
+
+const listPost = () => {
+    posts.map((post) => {
+        console.log(post)
+    })
+}
+
+const addPost = (newPost) => {
+
+    return new Promise((resolve, reject) => {
+        posts.push(newPost)
+        resolve("Post is added")
+        reject("Wrong!")
+    })
+
+}
+
+async function genPostControl (post) {
+    try {
+        await addPost(post)
+        listPost()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+genPostControl({title : "Post 4"})
