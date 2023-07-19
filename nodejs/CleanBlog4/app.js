d45ca18b274c99c76dae8c8202117ab23333be66
Furkan Özay <53002170+FurkanOzay@users.noreply.github.com>
2023-07-20 00:47:20 +0300

    Add files via upload

---
+++ nodejs/CleanBlog4/app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const ejs = require('ejs');
+const path = require('path');
+const Photo = require('./models/Photo')
+
+const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/pcat-test-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+const port = 3001;
+
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({})
+  res.render('index', {
+    photos
+  });
+});
+
+app.get('/photos/:id', async (req, res) => {
+  //console.log(req.params.id)
+  //res.render('about');
+  const photo = await Photo.findById(req.params.id)
+  res.render('photo', {
+    photo
+  })
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+
+app.post('/photos', async (req,res) => {
+  await Photo.create(req.body)
+  res.redirect('/')
+})
+
+
+
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda çalıştı`);
+});
